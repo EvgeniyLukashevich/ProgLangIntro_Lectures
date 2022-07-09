ce968b46b176acdeca31b294e239f3f537abaa40
LukashevichEvgeniy <humansyndr8me@gmail.com>
2022-07-09 15:26:16 +0800

    example005  condition_if_else added

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ого!!! Чё-каво, Мария?");
+}
+else
+{
+    Console.Write("Добрый день, ");
+    Console.WriteLine(username);
+}
