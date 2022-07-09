4f88883350f98556e12326c8383964fc6d51218b
LukashevichEvgeniy <humansyndr8me@gmail.com>
2022-07-09 16:01:16 +0800

    example006 base added

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 3;
+int b = 8;
+int c = 1;
+int d = 6;
+int e = 11;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.Write(max);
