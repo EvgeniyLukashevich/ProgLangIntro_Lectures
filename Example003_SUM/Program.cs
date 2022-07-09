718963fefe63d3c66ef054cbd0e2a9b1570cfe23
LukashevichEvgeniy <humansyndr8me@gmail.com>
2022-07-09 14:46:37 +0800

    random added

--- Example003_SUM/Program.cs
+++ Example003_SUM/Program.cs
@@ -1,4 +1,8 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // 0 1 2 3 4 ... 9
+int numberB = new Random().Next(10, 51); // 10, 11, 12 ... 50
 int result = numberA + numberB;
-Console.WriteLine(result);
+Console.Write(numberA);
+Console.Write(" + ");
+Console.Write(numberB);
+Console.Write(" = ");
+Console.Write(result);
